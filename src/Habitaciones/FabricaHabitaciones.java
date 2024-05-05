73a6fb537d7b20a6a17316e7258aaa0b5fa9865d
Luis a.k.a. Wito <lrl0015@alu.medac.es>
2024-05-05 20:09:30 +0200

    Empezadas habitaciones

---
+++ src/Habitaciones/FabricaHabitaciones.java
@@ -0,0 +1,5 @@
+package habitaciones;
+
+public class FabricaHabitaciones {
+
+}
---
+++ src/Habitaciones/Habitacion.java
@@ -0,0 +1,12 @@
+package habitaciones;
+
+public class Habitacion {
+
+    private String descripcion;
+    private String[] dialogo;
+    private String[] pistas;
+    //Por implementar
+    //private Inventario[] inventario;
+    //private Enemigo enemigo;
+
+}
